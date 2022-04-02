76b27b4481cf42d370085b089d2e8c6408257a49
Seunghoon Jung <hks026naver@gmail.com>
2022-04-02 10:20:48 +0900

    first commit

---
+++ 소스.c
@@ -0,0 +1,208 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+#include <conio.h>
+
+int main(void) {
+
+	system("color 70");
+
+	Sleep(1000);
+
+	printf("\n\n\n");
+
+	for (int i = 0; i <= 85; i++) {  //첫 줄 띄어쓰기
+		printf(" ");
+	}
+
+	printf("자리 배치를 시작합니다");  //문구 출력
+
+	Sleep(500);
+
+	int p, x, z;
+
+re:;
+	printf("\n\n\n");
+
+	for (int i = 0; i <= 85; i++) printf(" ");
+
+	printf("인원 수를 입력해주세요 : ");
+
+	scanf("%d", &p);
+
+	Sleep(500);
+
+	printf("\n\n");
+
+	for (int i = 0; i <= 85; i++) printf(" ");
+
+	printf("줄 수를 입력하세요 : ");
+
+	scanf("%d", &x);
+
+	int i, r, j, random[100];
+
+	Sleep(500);
+
+	srand(time(NULL));   //랜덤함수 초기화 구문
+
+	system("cls");
+
+	for (int i = 0; i <= 15; i++) printf("\n");
+
+	printf("\t\t\t\t\t\t\t                         □□□□□□□□□□\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                         □□□□□□□□□□\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                         □□□□□□□□□□\n\n\n\n\n\n\n\n\n\n\n");
+
+	Sleep(1000);
+
+	system("cls");
+
+	for (int i = 0; i <= 15; i++) printf("\n");
+
+	printf("\t\t\t\t\t\t\t                         □□□□□□□□□□\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                         □□□□□□□□□□\n");
+	printf("\t\t\t\t\t\t\t                         □\n");
+	printf("\t\t\t\t\t\t\t                         □\n");
+	printf("\t\t\t\t\t\t\t                         □\n");
+	printf("\t\t\t\t\t\t\t                         □\n");
+	printf("\t\t\t\t\t\t\t                         □\n");
+	printf("\t\t\t\t\t\t\t                         □\n");
+	printf("\t\t\t\t\t\t\t                         □\n");
+	printf("\t\t\t\t\t\t\t                         □\n");
+	printf("\t\t\t\t\t\t\t                         □\n");
+	printf("\t\t\t\t\t\t\t                         □□□□□□□□□□\n\n\n\n\n\n\n\n\n\n\n");
+
+
+
+	Sleep(1000);
+
+	system("cls");
+
+	for (int i = 0; i <= 15; i++) printf("\n");
+
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n");
+	printf("\t\t\t\t\t\t\t                                           □\n\n\n\n\n\n\n\n\n\n\n");
+
+	Sleep(1000);
+
+	system("cls");
+
+	for (int i = 0; i <= 20; i++) printf("\n");
+
+	for (i = 0; i <= p - 1; i++) {     //랜덤 출력 for문
+	again:;                          //반복 위치 선언
+		r = rand() % p + 1; //난수 선언
+
+		for (j = 0; j <= i - 1; j++) //중복 제거 for문
+
+			if (r == random[j])
+
+				goto again;
+
+		random[i] = r;
+	}
+	for (int i = 0; i <= 85; i++) printf(" "); //교탁 위치 추가
+
+	printf("┌  칠판  ┐");
+
+	for (i = 0; i <= p - 1; i++) {
+		if (i % x == 0) {
+
+			printf("\n\n\n"); //줄바꿈
+
+			for (int i = 0; i <= 65; i++) {
+				printf(" ");        //각 줄의 공백
+			}
+		}
+
+		printf("   ┌  %d ┐  ", random[i]); //자리 출력
+
+		Sleep(250);
+	}
+	Sleep(500);    //잠깐 대기
+
+	printf("\n\n\n\n\n"); // 마지막 공백
+
+	for (int i = 0; i <= 72; i++) printf(" ");
+
+	printf("종료하시겠습니까?? [yes = 1, no = 0] : ");
+
+gg:;
+	scanf("%d", &z);
+
+	printf("\n\n\n");
+
+	if (z == 0) {
+		system("cls");
+		goto re;
+	}
+	else if (z == 1) {
+		for (int i = 0; i <= 82; i++) printf(" ");
+		printf("3초후에 종료합니다");
+		printf(".");
+		Sleep(1000);
+		printf(".");
+		Sleep(1000);
+		printf(".");
+		Sleep(1000);
+	}
+	else {
+		for (int i = 0; i <= 75; i++) printf(" ");
+		printf("다시 입력하세욧 [yes = 1, no = 0] : ");
+		goto gg;
+	}
+
+	printf("\n\n\n");
+
+	system("pause");
+
+	return 0;
+}
